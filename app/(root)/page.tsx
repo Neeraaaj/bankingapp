c8c80167602c4339be365c2ae7ab574c18804169
Lucifer <patilneeraj2003@gmail.com>
2024-06-04 17:39:38 +0530

    implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,40 @@
+import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
+import { Sidebar } from 'lucide-react'
 import React from 'react'
+type BankAccount = {
+    currentBalance: number
+}
 
 const Home = () => {
+    const loggedIn  = {firstName: 'Neeraj', lastName: 'Patil', email: 'patilneeraj2003@gmail.com'} as User;
   return (
-    <div>Home</div>
+    <section className='home'>
+        <div className='home-content'>
+            <header className='home-header'>
+                <HeaderBox 
+                    type="greeting"
+                    title="Welcome"
+                    user = {loggedIn?.firstName || 'Guest'}
+                    subtext = "Access and manage your account and transactions efficiently"
+                />
+
+                <TotalBalanceBox 
+                    accounts = {[]}
+                    totalBanks = {1}
+                    totalCurrentBalance={1250.35}
+                />
+
+            </header>
+            RECENT TRANSACTIONS
+        </div>
+        <RightSidebar 
+            user={loggedIn}
+            transactions={[]}
+            banks={[{currentBalance: 123.50}, {currentBalance: 126.90}] as (Bank & Account)[]} 
+        />
+    </section>
   )
 }
 
